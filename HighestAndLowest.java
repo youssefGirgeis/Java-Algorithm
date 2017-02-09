a8b41fdf794a74eb10cfcff05039119501996cec
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-02-09 03:17:32 -0500

    add description

--- HighestAndLowest.java
+++ HighestAndLowest.java
@@ -1,3 +1,11 @@
+//In this little assignment you are given a string of space separated numbers,
+// and have to return the highest and lowest number.
+//There will always be at least one number in the input string.
+//Output string must be two numbers separated by a single space, and highest number is first.
+
+
+
+
 import java.util.Arrays;
 
 
